9575e4abddd1b45f77c7fd8103215c2e4411bf8a
Tiago Lima <tiago.jlima@yahoo.com.br>
2016-03-15 18:10:00 -0300

    integrando site com sistema ajax

--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 var express = require('express')
   , bodyParser = require('body-parser')
   , cookieParser = require('cookie-parser')
+  , methodOverride = require('method-override')
   , helmet = require('helmet')
   , load = require('express-load')
   , favicon = require('static-favicon');
@@ -13,7 +14,7 @@ module.exports = function(){
   app.use(express.static('./public'));
 
   app.use(function(req, res, next){
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
@@ -23,17 +24,18 @@ module.exports = function(){
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
   app.use(cookieParser());
-  app.use(require('method-override')());
+  app.use(methodOverride());
 
   app.use(helmet());
   app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }));
   app.disable('x-powered-by');
   app.use(helmet.ieNoOpen());
 
-  load('models', {cwd: 'app'})
-    .then('controllers')
-    .then('routes')
-    .into(app);
+
+ load('models', {cwd: 'app'})
+  .then('controllers')
+  .then('routes')
+  .into(app);
 
   return app;
 };
