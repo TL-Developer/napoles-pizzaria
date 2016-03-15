73f714a3b497e6b2a48899d5d5eb93e983d920fb
Tiago Lima <tiago.jlima@yahoo.com.br>
2016-03-15 18:07:16 -0300

    integrando site com sistema ajax

--- config/express.js
+++ config/express.js
@@ -13,7 +13,7 @@ module.exports = function(){
   app.use(express.static('./public'));
 
   app.use(function(req, res, next){
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
